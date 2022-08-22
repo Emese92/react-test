16046480fec9802b64b6885403b280645fa7bd82
Emese92 <gregor.emese@gmail.com>
2022-08-22 19:03:08 +0000

    Adding content with 3 empty links

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import App from "../App";
+
+import React from "react";
+
+const Sidebar = (props) => {
+
+    return <div>
+        <a>My Photos</a>
+        <a>My Illustrations</a>
+        <a>My Pantings</a>
+    </div>;
+
+
+}
+
+export default Sidebar;
