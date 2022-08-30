9615dd8f671f0fd015276ed23572e3333986eb2d
Emese92 <gregor.emese@gmail.com>
2022-08-30 15:27:03 +0000

    Add NavBarForm to test handling events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
-import NavBarSimple from "./components/NavBarSimple";
+// import NavBarSimple from "./components/NavBarSimple";
+import NavbarForm from "./components/NavBarForm"
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarSimple />
-      <Sidebar />
+      {/* <NavBarSimple /> */}
+      {/* <Sidebar /> */}
+      <NavbarForm />
     </div>
   );
 }
