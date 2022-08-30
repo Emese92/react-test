08bf7d92db95f648611d6f18040d84e9e0c23a31
Emese92 <gregor.emese@gmail.com>
2022-08-30 22:57:58 +0000

    Rendering lists challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,17 @@ import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
 // import NavBarSimple from "./components/NavBarSimple";
 import NavbarForm from "./components/NavBarForm"
+import Content from "./components/Content"
+
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
       {/* <NavBarSimple /> */}
-      {/* <Sidebar /> */}
+      <Sidebar />
       <NavbarForm />
+      <Content />
     </div>
   );
 }
