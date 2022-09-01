b93ae92061a869310e8405bc08c51b1ad5de0994
Emese92 <gregor.emese@gmail.com>
2022-09-01 00:54:16 +0000

    Add searchbar to content

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
 import css from "./css/Content.module.css";
-import {savedPosts} from '../posts.json';
+import { savedPosts } from '../posts.json';
 import PostItem from './PostItem'
+import Loader from './Loader';
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isLoaded: false,
+            posts: [],
+        }
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+                posts: savedPosts,
+            })
+        }, 2000)
+    }
+
+    handleSearch = (e) => {
+        const name = e.target.value.toLowerCase()
+        const fiteredPosts = savedPosts.filter((post) => {
+            return post.name.toLowerCase().includes(name);
+        })
+        this.setState({
+            posts: fiteredPosts
+        })
 
     }
     render() {
         return (
             <div className={css.Content}>
-                <div className={css.TitleBar}><h1>My Photos</h1></div>
-                <div className={css.SearchResults}><PostItem savedPosts={savedPosts}/></div>
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor="sarchinput">Search</label>
+                        <input
+                            type="search"
+                            id="searchinput"
+                            placeholder="By Author"
+                            onChange={(e) => this.handleSearch(e)} />
+                        <h4>Posts Found: {this.state.posts.length}</h4>
+                    </form>
+                </div>
+                <div className={css.SearchResults}>
+                    {
+                        this.state.isLoaded ?
+                            <PostItem savedPosts={savedPosts} /> : <Loader />
+                    }
+                </div>
             </div>
         )
     }
